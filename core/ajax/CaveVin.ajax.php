f367dd4b5499d8f983b3c0449e48e0eac500e06c
Halbout Michael <michaelhalbout@gmail.com>
2016-09-23 10:20:42 +0200

    Update CaveVin.ajax.php

--- core/ajax/CaveVin.ajax.php
+++ core/ajax/CaveVin.ajax.php
@@ -1,21 +1,4 @@
 <?php
-
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-
 try {
 	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
     include_file('core', 'authentification', 'php');
@@ -34,16 +17,16 @@ try {
 				$return = utils::o2a(mesVin::all());
 			break;
 		}
-	/*	for($loop=0,$loop<=count($return),$loop++){
+		for($loop=0,$loop<=count($return),$loop++){
 			$return[$loop]['QtsTypeVin']=0;
 			$Caves=eqLogic::byType('CaveVin');
 			if (is_object($Caves))
 			{
 				foreach ($Caves as $Cave){
-					$return$return[$loop]['QtsTypeVin']+=count($Cave->getCmd(null, $return['id']));
+					$return[$loop]['QtsTypeVin']+=count($Cave->getCmd(null, $return['id']));
 				}
 			}
-		}*/
+		}
         ajax::success(jeedom::toHumanReadable($return));
 	}
 	if (init('action') == 'getVinInformation') {	
