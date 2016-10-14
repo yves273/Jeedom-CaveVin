c4a7ef7a2d605bc929577ee5799fdd9030a1528e
Halbout Michael <michaelhalbout@gmail.com>
2016-10-14 09:12:00 +0200

    Update CaveVin.php

--- desktop/php/CaveVin.php
+++ desktop/php/CaveVin.php
@@ -72,8 +72,10 @@ $eqLogics = eqLogic::byType('CaveVin');
 				<div class="form-group">
 					<label class="col-sm-3 control-label" ></label>
 					<div class="col-sm-9">
-						<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-						<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+						<label>{{Activer}</label>
+						<input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+						<label>{{Visible}</label>
+						<input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
 					</div>
 				</div>
                 <div class="form-group">
